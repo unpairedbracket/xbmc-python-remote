46b44be51d66d77b86fd2d898c8efc43db54f0fe
NamelessOne2345 <ben.spiers22@gmail.com>
2011-08-21 23:16:22 +0100

    Refactor Actions.py

--- Actions.py
+++ Actions.py
@@ -4,31 +4,34 @@ Created on Aug 17, 2011
 @author: benspiers
 '''
 
+import socket
+import select
+
 class Actions(object):
     '''
     classdocs
     '''
 
-
     def __init__(self):
         '''
         Constructor
         '''
-        return
-    
-    def getSocket(self, IpAddress, Port):
-        
-        import socket
+        from XBMCJsonObjects import XBMCJson
+        self.XJ = XBMCJson()
         #create an INET, STREAMing socket
         self.__s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
+    def getSocket(self, IpAddress, Port):
+        
         #now connect
-        self.__s.connect((IpAddress, Port))
+        try:
+            self.__s.connect((IpAddress, Port))
+            return True
+        except socket.error:
+            return False
         
     def closeSocket(self):
         
-        import socket
-
         self.__s.shutdown(socket.SHUT_RDWR)
         self.__s.close()
     
@@ -36,61 +39,52 @@ class Actions(object):
     
     def PlayPause(self):
         
-        from XBMCJson import XBMCJson
-
-        XJ = XBMCJson()
-
-        action = XJ.XBMC_PLAY
+        action = self.XJ.XBMC_PLAY
         self.__sendJson(action)
                 
     def PlayNext(self):
         
-        from XBMCJson import XBMCJson
-
-        XJ = XBMCJson()
-
-        action = XJ.XBMC_NEXT
+        action = self.XJ.XBMC_NEXT
         self.__sendJson(action)
                 
     def PlayPrevious(self):
         
-        from XBMCJson import XBMCJson
-
-        XJ = XBMCJson()
-
-        action = XJ.XBMC_PREV
+        action = self.XJ.XBMC_PREV
         self.__sendJson(action)
                 
     def StartPlaying(self):
         
-        from XBMCJson import XBMCJson
-
-        XJ = XBMCJson()
-
-        action = XJ.XBMC_START
+        action = self.XJ.XBMC_START
         self.__sendJson(action)
                 
     def StopPlaying(self):
         
-        from XBMCJson import XBMCJson
-
-        XJ = XBMCJson()
-
-        action = XJ.XBMC_STOP
+        action = self.XJ.XBMC_STOP
         self.__sendJson(action)
                 
     def __sendJson(self, JsonObject):
-        import select
 
         action = JsonObject
         self.__s.send(bytes(action, 'UTF-8'))
 
+        self.__logResponse()
+        
+    def __logResponse(self):    
+        import logging
+        log = logging.getLogger("responses")
         # Print the results
         while True:
             response = self.__s.recv(0x4000).decode('UTF-8')
-            print(response)
+            log.debug(response)
     
             if len(select.select([self.__s], [], [], 0)[0]) == 0:
                 break;
                 
-            
+    def __returnResponse(self):    
+        response = []
+        # Print the results
+        while True:
+            response.append(self.__s.recv(0x4000)).decode('UTF-8')
+    
+            if len(select.select([self.__s], [], [], 0)[0]) == 0:
+                return response;
