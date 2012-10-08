a88b0e81ce8e972517e2a130a4d50766ae6150ab
Ben Spiers <ben.spiers22@gmail.com>
2012-10-08 10:08:36 +0100

    Put a validation check into the preferences window

--- xbmcremote/frontends/GtkFrontend/PreferencesXbmcremoteDialog.py
+++ xbmcremote/frontends/GtkFrontend/PreferencesXbmcremoteDialog.py
@@ -23,8 +23,9 @@ See http://developer.gnome.org/gio/stable/GSettings.html for more info.
 '''
 
 import logging
+from socket import gethostbyaddr, herror, gaierror
 
-from gi.repository import Gio  # pylint: disable=E0611
+from gi.repository import Gio, Gtk  # pylint: disable=E0611
 
 from xbmcremote_lib.PreferencesDialog import PreferencesDialog
 
@@ -47,6 +48,7 @@ class PreferencesXbmcremoteDialog(PreferencesDialog): # pylint: disable=W0232
         port_widget = self.builder.get_object('port_entry')
         version_widget = self.builder.get_object('version_combo')
         mpris_widget = self.builder.get_object('mpris2_check')
+        settings.connect('changed', self.on_preferences_changed)
         settings.bind('ip-address', ip_widget,
                       'text', Gio.SettingsBindFlags.DEFAULT)
         settings.bind('port', port_widget,
@@ -57,3 +59,41 @@ class PreferencesXbmcremoteDialog(PreferencesDialog): # pylint: disable=W0232
                       'active', Gio.SettingsBindFlags.DEFAULT)
 
         # Code for other initialization actions should be added here.
+
+    def on_preferences_changed(self, settings, key, data=None):
+        '''Validate a change in preferences'''
+        if key == 'ip-address':
+            # Validate IP address
+            try:
+                gethostbyaddr(settings.get_string('ip-address'))
+            except gaierror:
+                logger.debug("Can't resolve host")
+                self.ui.invalid_host_label.set_text("Can't resolve host")
+                self.ui.invalid_host_label.show()
+                self.ui.host_icon.set_from_stock(Gtk.STOCK_NO, 4)
+            except herror:
+                logger.debug('Invalid host')
+                self.ui.invalid_host_label.set_text("Invalid host")
+                self.ui.invalid_host_label.show()
+                self.ui.host_icon.set_from_stock(Gtk.STOCK_NO, 4)
+            else:
+                logger.debug('Valid host')
+                self.ui.invalid_host_label.hide()
+                self.ui.host_icon.set_from_stock(Gtk.STOCK_YES, 4)
+        elif key == 'port':
+            # Validate port number
+            try:
+                if 0 < int(settings.get_string('port')) < 65535:
+                    logger.debug('Port number in range')
+                    self.ui.invalid_port_label.hide()
+                    self.ui.port_icon.set_from_stock(Gtk.STOCK_YES, 4)
+                else:
+                    logger.debug('Port number out of range')
+                    self.ui.invalid_port_label.set_text('Port number must be between 1 and 65535')
+                    self.ui.invalid_port_label.show()
+                    self.ui.port_icon.set_from_stock(Gtk.STOCK_NO, 4)
+            except ValueError:
+                logger.debug('Port number is not int')
+                self.ui.invalid_port_label.set_text('Port number must be an integer')
+                self.ui.invalid_port_label.show()
+                self.ui.port_icon.set_from_stock(Gtk.STOCK_NO, 4)
